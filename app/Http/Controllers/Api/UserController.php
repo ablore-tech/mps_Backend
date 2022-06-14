b43a19111a1c8a0410c97eb8d95b99f403886f39
Vanshaj Jain <jainvanshaj99@gmail.com>
2022-06-14 21:04:00 +0530

    Fixed City updation issue and updated user responses

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,38 +3,46 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\UserRequest\StoreCityRequest;
 use App\Http\Requests\Api\V1\UserRequest\UpdateUserRequest;
-use App\Http\Requests\StoreCityRequest;
 use App\Http\Resources\Api\V1\User\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function addCity(StoreCityRequest $request)
     {
-        $user = User::find($request->user_id);
-        $user->city_id = $request->city_id;
-        $user->save();
+        $user = User::findOrFail($request->user_id);
+        if($user)
+        {
+            $user->city_id = $request->city_id;
+            $user->save();
 
-        return response()->json([
-            $user,
-            200
-        ]);
+            return response()->json(["success" => true, "message" => 'City updated Successfully'], 200);
+        }
+        return response()->json(["success" => false, "message" => 'User not found'], 200);
     }
 
     public function show(User $user)
     {
-        return response()->json([new UserResource($user), 200]);
+        if($user)
+        {
+            return response()->json(["success" => true, "message" => new UserResource($user) ], 200);
+        }
+        return response()->json(["success" => false, "message" => "User not found" ], 200);
     }
 
     public function update(UpdateUserRequest $request, User $user)
     {
-        $user->name = $request->name;
-        $user->alternate_number = $request->alternate_number;
-        $user->email = $request->email;
-        $user->save();
-        
-        return response()->json([new UserResource($user), 200]);
+        if($user)
+        {
+            $user->name = $request->name;
+            $user->alternate_number = $request->alternate_number;
+            $user->email = $request->email;
+            $user->save();
+
+            return response()->json(["success" => true, "message" => 'User updated Successfully'], 200);
+        }
+        return response()->json(["success" => false, "message" => 'User updation failed'], 200);
     }
 }
