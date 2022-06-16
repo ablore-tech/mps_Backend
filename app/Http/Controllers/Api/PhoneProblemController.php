96f9b871167099ea86f9ac0d6918490ef6af096a
Vanshaj Jain <jainvanshaj99@gmail.com>
2022-06-16 20:30:48 +0530

    Removed extra code

--- app/Http/Controllers/Api/PhoneProblemController.php
+++ app/Http/Controllers/Api/PhoneProblemController.php
@@ -16,8 +16,6 @@ class PhoneProblemController extends Controller
      */
     public function index($device_id)
     {
-
-        $phoneProblems = 
         $phoneProblems = PhoneProblem::with(['phoneProblemOptions' =>function($q) use($device_id) {
             $q->with(['devicePhoneProblemPrices' => function($query) use($device_id) {
                 $query->where('device_id', $device_id);
