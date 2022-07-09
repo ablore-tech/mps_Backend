a1213505650fb8d2b28e768ba4c59f5cb12c80c5
Vanshaj Jain <jainvanshaj99@gmail.com>
2022-07-09 16:36:25 +0530

    Updated role migration

--- database/migrations/2022_07_06_164002_add_role_id_column_in_users_table.php
+++ database/migrations/2022_07_06_164002_add_role_id_column_in_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->foreignId('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 
@@ -26,7 +26,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
         });
     }
 };
